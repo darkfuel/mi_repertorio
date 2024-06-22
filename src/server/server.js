09db86580085c945d7f41a98b0f152a17fa5d2f3
Abel Rivas Bautista <abel.rivas@Imperial.local>
2024-06-22 18:53:15 -0400

    Get, Post, Put & Delete ready

--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,18 @@
 import express from 'express'
 import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
 const app = express()
 const PORT = process.env.PORT ?? 3000
 
 app.use(express.json())
+
+app.get('/', (req, res) => {
+  const __filename = fileURLToPath(import.meta.url)
+  const __dirname = path.dirname(__filename)
+  res.sendFile(__dirname + '/index.html')
+})
 
 app.get('/canciones', (req, res) => {
   // obtener data
@@ -18,6 +26,11 @@ app.get('/canciones', (req, res) => {
 
 app.post('/canciones', (req, res) => {
   try {
+    const { id, titulo, artista, tono } = req.body
+    console.log(req.body, titulo)
+    if (id === undefined || titulo === undefined || artista === undefined || tono === '') {
+      throw new Error('Todos los campos son obligatorios')
+    }
     const cancion = req.body
     // obtener data
     const canciones = JSON.parse(fs.readFileSync('data/repertorio.json'))
@@ -31,8 +44,43 @@ app.post('/canciones', (req, res) => {
   }
 })
 
-app.put('/canciones/:id', (req, res) => { })
-app.delete('/canciones/:id', (req, res) => { })
+app.put('/canciones/:id', (req, res) => {
+  try {
+    const { id } = req.params
+    const cancion = req.body
+    const canciones = JSON.parse(fs.readFileSync('data/repertorio.json'))
+    // filtar canción a actualizar
+    const index = canciones.findIndex((cancion) => cancion.id === id)
+    // validación de existencia
+    if (index === -1) {
+      res.status(404).send('El ítem seleccionado no existe')
+    } else {
+    // reescribir registro
+      canciones[index] = cancion
+      fs.writeFileSync('data/repertorio.json', JSON.stringify(canciones))
+      res.status(200).json({ message: 'Canción actualizada' })
+    }
+  } catch (error) {
+    res.status(501).json({ menssage: `No se pudo actualizar la cancion ${error.message}` })
+  }
+})
+app.delete('/canciones/:id', (req, res) => {
+  try {
+    const { id } = req.params
+    // const cancion = req.body
+    const canciones = JSON.parse(fs.readFileSync('data/repertorio.json'))
+    // filtrar canción para eliminar
+    const index = canciones.findIndex((cancion) => cancion.id === id)
+    // validación y recorte de arreglo
+    index === -1
+      ? res.status(404).send('El ítem seleccionado no existe')
+      : (canciones.splice(index, 1),
+        fs.writeFileSync('data/repertorio.json', JSON.stringify(canciones)),
+        res.status(200).json({ message: 'Canción eliminada' }))
+  } catch (error) {
+    res.status(501).json({ menssage: `No se pudo actualizar la cancion ${error.message}` })
+  }
+})
 app.all('/*', (req, res) => res.json({ code: 404, message: 'URL no existe' }))
 
 app.listen(PORT, () => console.log('servidor inciado...'))
